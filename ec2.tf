3b594965364402775727170c793d5802a60a7323
Francois <fordeogold@gmail.com>
2024-05-27 17:44:13 +0100

    new files modified

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "server1" {
   ami = "ami-0d94353f7bad10668"
-  instance_type = "t2.micro"
+  instance_type = "var.INSTANCE_TYPE"
   vpc_security_group_ids = [ aws_security_group.sg1.id ]
-  availability_zone = "us-east-1a"
+  availability_zone = "${var.REGION}a"
   subnet_id = aws_subnet.private1.id
   user_data = file("code.sh")
   tags = {
-    Name = "webserver-1"
+    Name = var.ENVIRONMENT
   }
 
 }
@@ -14,8 +14,8 @@ resource "aws_instance" "server2" {
   ami = "ami-0d94353f7bad10668"
   instance_type = "t2.micro"
   vpc_security_group_ids = [ aws_security_group.sg1.id ]
-  availability_zone = "us-east-1a"
-  subnet_id = aws_subnet.private1.id
+  availability_zone = "us-east-1b"
+  subnet_id = aws_subnet.private2.id
   user_data = file("code.sh")
   tags = {
     Name = "webserver-2"
